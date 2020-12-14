eb9f541411201a8a503b1dabfff5529a35a3ff53
zhurk746 <71022138+zhurk746@users.noreply.github.com>
2020-12-14 14:21:41 -0500

    adding final project

---
+++ script.js
@@ -0,0 +1,6 @@
+d3.csv("/Desktop/DVIA Fall 2020/LooksData.csv", function(data){
+    console.log(data)
+});
+d3.selectAll('h2').style("color","purple")
+d3.selectAll('h3').style("color", "purple")
+d3.selectAll('p').style("color", "pink")
